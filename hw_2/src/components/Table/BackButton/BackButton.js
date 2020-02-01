4ebcbda88e5f02fe0d8751aca80e1a3a772ca352
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-02-01 10:29:15 +0200

    fix mistake in BackButton

--- hw_2/src/components/Table/BackButton/BackButton.js
+++ hw_2/src/components/Table/BackButton/BackButton.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default ({backButtonHandler}) => <button type="button" class="btn btn-lg btn-primary" onClick = {backButtonHandler}>←</button>;
+export default ({backButtonHandler}) => <button type="button" className="btn btn-lg btn-primary" onClick = {backButtonHandler}>←</button>;

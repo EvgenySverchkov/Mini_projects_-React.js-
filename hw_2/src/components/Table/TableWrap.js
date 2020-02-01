7c3d9fbddfc3751c0e9d00a1d07c46c029884596
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-02-01 10:08:15 +0200

    added index.js for WarnScreen

--- hw_2/src/components/Table/TableWrap.js
+++ hw_2/src/components/Table/TableWrap.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 
 import UserTable from './UserTable.js';
 import BackButton from './BackButton';
-import InfoAboutEmptyData from './WarnScreen/InfoAboutEmptyRequestScreen.js';
+import InfoAboutEmptyData from './WarnScreen';
 import dataUserRequest from '../../services/requestForUsers.js';
 
 const TableWrap = ({userId, backBtnHandler})=>{

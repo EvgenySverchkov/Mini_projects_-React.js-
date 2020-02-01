3cc70627fb1b33810f6e4330593914dff0007dd2
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-02-01 09:50:29 +0200

    little changes

--- hw_2/src/components/Profile.js
+++ hw_2/src/components/Profile.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Profile = ({heandler}) => (
-	
-		<form style={wrapStyle} className="form-inline" onSubmit = {heandler}>
-			<div className="form-group mx-sm-3 mb-2">
-				<input className="form-control" id="userId" type="text" min="0" max="20" placeholder="Enter user's id"/>
-			</div>
-			<input type="submit" className="btn btn-primary mb-2" value="Submit"/>
-		</form>
-
+	<form style={wrapStyle} className="form-inline" onSubmit = {heandler}>
+		<div className="form-group mx-sm-3 mb-2">
+			<input className="form-control" id="userId" type="text" min="0" max="20" placeholder="Enter user's id"/>
+		</div>
+		<input type="submit" className="btn btn-primary mb-2" value="Submit"/>
+	</form>
 );
 
 const wrapStyle = {

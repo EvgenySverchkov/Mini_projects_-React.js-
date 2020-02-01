92880ba62985d595190d0440868b98df5485c618
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-02-01 10:41:59 +0200

    added PropTypes to Profile.js

--- hw_2/src/components/Profile/Profile.js
+++ hw_2/src/components/Profile/Profile.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Profile = ({heandler}) => (
 	<form style={wrapStyle} className="form-inline" onSubmit = {heandler}>
 		<div className="form-group mx-sm-3 mb-2">
-			<input className="form-control" id="userId" type="text" min="0" max="20" placeholder="Enter user's id"/>
+			<input className="form-control" id="userId" type="number" placeholder="Enter user's id"/>
 		</div>
 		<input type="submit" className="btn btn-primary mb-2" value="Submit"/>
 	</form>
@@ -12,5 +13,9 @@ const Profile = ({heandler}) => (
 const wrapStyle = {
 	width: '30%',
 	margin: '0 auto'
+}
+
+Profile.propTypes = {
+	heandler: PropTypes.func
 }
 export default Profile;
